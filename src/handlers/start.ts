fc7b3c321fadcacbda10474b069f4f7f8d650753
amenabe22 <bdere12345@gmail.com>
2022-07-16 22:33:51 +0300

    added basic localization setup

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -3,7 +3,6 @@ import { englishMainMenuKeyboard } from "../keybaords/menu_kbs"
 import { fetchJob } from "../services/jobpost"
 
 export const startCommandHand = async (ctx: any) => {
-    console.log(ctx.from.id)
     console.log("******\n\n", ctx.message.text, "\n\n******")
     const startcmd = ctx.message.text.split(" ")
     const withmsg = startcmd.length > 1
@@ -11,7 +10,7 @@ export const startCommandHand = async (ctx: any) => {
     const { data: { users } } = await getUserByTelegramId({
         telegram_id: JSON.stringify(ctx.from.id),
     })
-    console.log(users,"usr")
+    console.log(users, "usr")
     if (!users.length) {
         return ctx.scene.enter("newCustomerRegistrationScene");
     } else {
@@ -28,17 +27,20 @@ export const startCommandHand = async (ctx: any) => {
                 console.log("error fetching job")
             })
         }
-     
+
         console.log("POST: ", jobpostvalid)
         if (jobpostvalid) {
             ctx.session.postId = startcmd[1]
-            console.log("title",ctx.session.jobPostTitle);
-            console.log("des",ctx.session.jobPostDescription);
+            console.log("title", ctx.session.jobPostTitle);
+            console.log("des", ctx.session.jobPostDescription);
             return ctx.scene.enter("jobApplicationScene")
         } else {
             const [usr] = users
             let firstName = usr.first_name;
-            ctx.replyWithHTML(`hi ${firstName}, please select which one of you are ?`, englishMainMenuKeyboard);
+            const greeting = ctx.i18n.t('greeting', {
+                firstName
+            })
+            ctx.replyWithHTML(greeting, englishMainMenuKeyboard);
         }
     }
 }
